c19cdd551b122eb917a49ec182205bc321e7621f
Hyeong gil <gudrlf7913@naver.com>
2022-01-30 07:13:50 +0900

    first commit

---
+++ backend/server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const db = require("./db");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+/*db.pool.query(`CREATE TABLE lists (
+    id INTEGER AUTO_INCREMENT,
+    value TEXT,
+    PRIMARY KEY (id)
+)`, (err, results, fileds) => {
+    console.log('results', results);
+})*/
+
+app.get('/api/values', (req, res) => {
+    db.pool.query('SELECT * FROM lists;', (err, results, fileds) => {
+        if(err) {
+            return res.status(500).send(err)
+        }else {
+            return res.json(results)
+        }
+    });
+});
+
+app.post('/api/value', (req, res, next) => {
+    db.pool.query(`INSERT INTO lists (value) VALUES("${req.body.value}")`, (err, results, fileds) => {
+        if(err) {
+            return res.status(500).send(err)
+        }else {
+            return res.json({
+                success: true,
+                value: req.body.value
+            })
+        }
+    });
+});
+
+app.listen(5000, () => {
+    console.log("어플리케이션 5000번 포트에서 작동")
+});
